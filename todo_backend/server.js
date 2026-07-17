eed229e5e313435b40f38399648c6514ce9752a2
Yashwanth <namburi.yashwanth@gmail.com>
2026-07-18 00:09:15 +0530

    Add GitHub Actions CI workflow

--- todo_backend/server.js
+++ todo_backend/server.js
@@ -55,9 +55,9 @@
 
 // module.exports=app;
 
-
-const mongoose = require('mongoose');
 const app = require('./app');
+const mongoose = require('mongoose');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/Todo')
   .then(() => console.log('MongoDB connected'))
